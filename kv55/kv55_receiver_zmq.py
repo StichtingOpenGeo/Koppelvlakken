381fc0e62b00cad1075a7c4e46cc8c51246ce7d3
Stefan de Konink <stefan@konink.de>
2012-01-24 03:41:57 +0100

    Implements adding of license, and removes the requirement for parsing the XML from GOVI.

--- kv55/kv55_receiver_zmq.py
+++ kv55/kv55_receiver_zmq.py
@@ -1,7 +1,7 @@
 import uwsgi
 import zmq
+import re
 from time import gmtime, strftime
-from xml.etree.cElementTree import XML
 
 from consts import KV55_RES, ZMQ_PUBSUB
 
@@ -9,20 +9,19 @@ context = zmq.Context()
 sender = context.socket(zmq.PUB)
 sender.bind(ZMQ_PUBSUB)
 
+match_timingpointcode = re.compile('<TimingPointCode>([0-9]+)</TimingPointCode>')
+
 def KV55(environ, start_response):
     reply = KV55_RES % {'timestamp': strftime("%Y-%m-%dT%H:%M:%S", gmtime())}
     start_response('200 OK', [('Content-Type', 'text/xml'), ('Content-length', str(len(reply)))])
-    
     try:
         response = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
-        #response = environ['wsgi.input'].read()
-        root = XML(response)
-        timingpointcode = root.find('.//TimingPointCode').text
+        timingpointcode = match_timingpointcode.search(response).group(1)
+        response = response.replace('<Request>',"<License>http://openov.nl/license/GOVI-1.0-TravelInfo</License>\n\t<Request>")
 
         sender.send('%(timingpointcode)s,%(response)s' % {'timingpointcode': timingpointcode, 'response': response})
     except SyntaxError:
         pass
-
     yield reply
 
 uwsgi.applications = {'':KV55}
