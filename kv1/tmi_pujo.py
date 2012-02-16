8158fe5423ad53c2ef401ad1b40af96b8ff54983
Stefan de Konink <stefan@konink.de>
2012-02-16 17:02:12 +0100

    Implements the right sequence for the foreign key to tive and timdemgrp.

--- kv1/tmi_pujo.py
+++ kv1/tmi_pujo.py
@@ -17,8 +17,8 @@ class tmi_pujo():
             {'name': 'WheelChairAccessible', 'aard': '+', 'type': 'B', 'length': 5 },
             {'name': 'DataOwnerIsOperator', 'aard': '+', 'type': 'B', 'length': 5 },
         ]
-        self.references = {'tive': ['TimetableVersionCode', 'OrganizationalUnitCode', 'PeriodGroupCode', 'SpecificDayCode'],
-                           'timdemgrp': ['LinePlanningNumber', 'TimeDemandGroupCode', 'JourneyPatternCode']}
+        self.references = {'tive': ['OrganizationalUnitCode', 'TimetableVersionCode', 'PeriodGroupCode', 'SpecificDayCode'],
+                           'timdemgrp': ['LinePlanningNumber', 'JourneyPatternCode', 'TimeDemandGroupCode']}
 
     def parse(self, version, implicit, data_owner_code, elements):
         timetable_version_code, organizational_unit_code, period_group_rode, specific_day_code, day_type, line_planning_number, journey_number, time_demand_group_code, journey_pattern_code, departure_time, wheel_chair_accessible, data_owner_is_operator = elements
@@ -26,7 +26,7 @@ class tmi_pujo():
         wheel_chair_accessible = wheelchair(wheel_chair_accessible)
         departure_time = time32(departure_time)
 
-        daytype = daytype2bitstring(day_type)
+        daytype = str(daytype2bitstring(day_type))
 
         self.data.append([version, implicit, data_owner_code, timetable_version_code, organizational_unit_code, period_group_rode, specific_day_code,
                           daytype, line_planning_number, journey_number, time_demand_group_code, journey_pattern_code,
