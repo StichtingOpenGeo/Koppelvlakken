35626f39f6ab29de7502e1cdd64e2f96e7809d0a
Skywave <thakoch@gmail.com>
2012-11-05 21:18:45 +0100

    Extend create-gtfs IFF script

--- iff/create-gtfs-iff.sh
+++ iff/create-gtfs-iff.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+DBNAME=iff
 DATE=$(date +'%Y%m%d')
 rm /tmp/*.txt
 
+dropdb $DBNAME
+createdb $DBNAME
+psql -d $DBNAME -c "create extension postgis;"
+python parser.py $1
+psql -d $DBNAME -f iff.sql
 cp stops_positioned.txt /tmp
-psql -d $1 -f iff-gtfs.sql
+psql -d $DBNAME -f iff-gtfs.sql
 zip -j gtfs-iffns-$DATE.zip /tmp/*.txt
